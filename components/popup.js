5cf66ccd10df392a5131bfb4c301cb217a8c9747
Mikhail Dushechkin <mishan92097@gmail.com>
2022-11-11 17:41:16 +0300

    feat: create popup function and edit profile

---
+++ components/popup.js
@@ -0,0 +1,38 @@
+let profile = document.querySelector('.profile');
+let profileEditButton = profile.querySelector('.profile__edit-button');
+let profileName = profile.querySelector('.profile__name');
+let profileDescription = profile.querySelector('.profile__description');
+
+let popUp = document.querySelector('.popup');
+let popUpCloseButton = popUp.querySelector('.popup__close-button');
+
+let editForm = document.querySelector('.edit-form');
+let inputName = editForm.querySelector('#profile-name');
+let inputDescription = editForm.querySelector('#profile-description');
+let saveFormButton = editForm.querySelector('.edit-form__save-button');
+
+function openPopUp() {
+  popUp.classList.add('popup_opened');
+
+  inputName.value = profileName.textContent;
+  inputDescription.value = profileDescription.textContent;
+
+  inputName.focus();
+}
+
+function closePopUp() {
+  popUp.classList.remove('popup_opened');
+}
+
+function saveForm(evt) {
+  evt.preventDefault();
+
+  profileName.textContent = inputName.value;
+  profileDescription.textContent = inputDescription.value;
+
+  closePopUp(evt);
+}
+
+profileEditButton.addEventListener('click', openPopUp);
+popUpCloseButton.addEventListener('click', closePopUp);
+saveFormButton.addEventListener('click', saveForm);
