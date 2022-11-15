0b3be01cb8d6a89754aaf38afdc3ad783d078354
Mikhail Dushechkin <mishan92097@gmail.com>
2022-11-15 23:27:33 +0300

    feat: Added popup with a form for adding photos, added a script for opening the form, added smoothness of opening/closing popup

--- components/index.js
+++ components/index.js
@@ -1,17 +1,30 @@
 let profile = document.querySelector('.profile');
 let profileEditButton = profile.querySelector('.profile__edit-button');
+let addPhotoButton = profile.querySelector('.profile__add-button');
 let profileName = profile.querySelector('.profile__name');
 let profileDescription = profile.querySelector('.profile__description');
 
-let popUp = document.querySelector('.popup');
-let popUpCloseButton = popUp.querySelector('.popup__close-button');
+let profileEditPopUp = document.querySelector('.edit-popup');
+let profileEditCloseButton = profileEditPopUp.querySelector('.popup-edit__close-button');
 
-let editForm = document.querySelector('.edit-form');
-let inputName = editForm.querySelector('#profile-name');
-let inputDescription = editForm.querySelector('#profile-description');
+let addPhotoPopUp = document.querySelector('.add-popup');
+let addPhotoCloseButton = addPhotoPopUp.querySelector('.popup-add__close-button');
 
-function openPopUp() {
-  popUp.classList.add('popup_opened');
+let profileEditForm = document.querySelector('.edit-form');
+let addPhotoForm = document.querySelector('.add-cards-form');
+let inputName = profileEditForm.querySelector('#profile-name');
+let inputDescription = profileEditForm.querySelector('#profile-description');
+
+function openPopUpAddPhoto() {
+  addPhotoPopUp.classList.add('popup_opened');
+}
+
+function closePopUpAddPhoto() {
+  addPhotoPopUp.classList.remove('popup_opened');
+}
+
+function openPopUpEditInfo() {
+  profileEditPopUp.classList.add('popup_opened');
 
   inputName.value = profileName.textContent;
   inputDescription.value = profileDescription.textContent;
@@ -19,19 +32,22 @@ function openPopUp() {
   inputName.focus();
 }
 
-function closePopUp() {
-  popUp.classList.remove('popup_opened');
+function closePopUpEditInfo() {
+  profileEditPopUp.classList.remove('popup_opened');
 }
 
-function saveForm(evt) {
+function saveProfileEditForm(evt) {
   evt.preventDefault();
 
   profileName.textContent = inputName.value;
   profileDescription.textContent = inputDescription.value;
 
-  closePopUp(evt);
+  closePopUpEditInfo();
 }
 
-profileEditButton.addEventListener('click', openPopUp);
-popUpCloseButton.addEventListener('click', closePopUp);
-editForm.addEventListener('submit', saveForm);
+profileEditButton.addEventListener('click', openPopUpEditInfo);
+profileEditCloseButton.addEventListener('click', closePopUpEditInfo);
+profileEditForm.addEventListener('submit', saveProfileEditForm);
+
+addPhotoButton.addEventListener('click', openPopUpAddPhoto);
+addPhotoCloseButton.addEventListener('click', closePopUpAddPhoto);
